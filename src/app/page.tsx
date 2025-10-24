75d50c8eb17a0d314e4002b6af7fdbaf7c8613ae
Ashwin Satheesh <ashwinsatheeshitsme@gmail.com>
2025-10-24 10:48:31 +0000

    nope still the same

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ import { PowerBiReports } from "@/components/sections/power-bi-reports";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col items-center">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 w-full">
         <Hero />
         <About />
         <PowerBiReports />
